4406708eb044e93f5dc6b914e7c994f5a1320dfc
SathvikSesha <lakshmisarmatk@gmail.com>
2026-05-29 12:31:24 +0530

    feat(backend): implement googleAuth controller for Google Sign-In

--- budget-backend/controllers/auth.controller.js
+++ budget-backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-
+import { OAuth2Client } from "google-auth-library";
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "7d",
@@ -16,6 +17,11 @@ export const registerUser = async (req, res) => {
 
     const userExists = await User.findOne({ email });
     if (userExists) {
+      if (userExists.authMethod !== "email") {
+        return res
+          .status(400)
+          .json({ error: "This email is registered using Google Sign-In" });
+      }
       return res.status(400).json({ error: "User already exists" });
     }
 
@@ -23,6 +29,7 @@ export const registerUser = async (req, res) => {
       name,
       email,
       password,
+      authMethod: "email",
     });
 
     if (user) {
@@ -45,9 +52,12 @@ export const loginUser = async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ error: "Missing fields" });
     }
-
     const user = await User.findOne({ email }).select("+password");
-
+    if (user.authMethod !== "email") {
+      return res.status(400).json({
+        error: "Please login using Google",
+      });
+    }
     if (user && (await user.matchPassword(password))) {
       res.json({
         _id: user._id,
@@ -58,6 +68,33 @@ export const loginUser = async (req, res) => {
     } else {
       res.status(401).json({ error: "Invalid email or password" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const googleAuth = async (req, res) => {
+  try {
+    const { credential } = req.body;
+    const ticket = await client.verifyIdToken({
+      idToken: credential,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await User.create({
+        name,
+        email,
+        authMethod: "google",
+      });
+    }
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
